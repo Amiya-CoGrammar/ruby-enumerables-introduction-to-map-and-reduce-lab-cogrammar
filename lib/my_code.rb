6ce3877023f59b3e785ae5b7c7446bd8e43dbd24
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-08 13:47:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,3 +48,17 @@ def reduce_to_all_true(x)
     return x
 end
 reduce_to_all_true([1,2,3])
+
+def reduce_to_all_true(x)
+  x.reduce { |var_true| if var_true == false; p false; else p true end}
+    return x
+end
+reduce_to_all_true([1,2,3])
+
+
+
+
+
+
+
+
