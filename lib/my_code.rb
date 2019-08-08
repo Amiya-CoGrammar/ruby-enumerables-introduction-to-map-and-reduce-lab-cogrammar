8121ac1680a30a7b8b9994feac1095808e677c2c
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-08 13:27:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -36,3 +36,8 @@ def reduce_to_total(x)
   x.reduce(0) { |sum, num| sum + num}
 end
 reduce_to_total([1,2,3])
+
+def reduce_to_total(x, starting_point)
+  x.reduce(0) {|sum, num| sum + num }
+end
+reduce_to_total([1,2,3], 100)
