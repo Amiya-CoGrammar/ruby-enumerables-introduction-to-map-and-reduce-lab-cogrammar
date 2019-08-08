2bc16d67e55fa4aa05003f0401cb90c33a7a56e7
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-08 13:31:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,10 +32,6 @@ end
 map_to_square([1,2,3])
 
 ########## reduce methods ##########
-def reduce_to_total(x)
-  x.reduce(0) { |sum, num| sum + num}
-end
-reduce_to_total([1,2,3])
 
 
 def reduce_to_total(x, starting_point)
